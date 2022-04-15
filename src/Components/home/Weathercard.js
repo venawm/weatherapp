1c21d98c22f13e235450dc97845c8b75f4333dc8
Samir <pandeysamir529@gmail.com>
2022-04-15 20:13:12 +0545

    Version 1.1  changes(1)Added styled components css still needs to be done )

--- src/Components/home/Weathercard.js
+++ src/Components/home/Weathercard.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import {useDispatch,useSelector} from 'react-redux'
+import styled from 'styled-components'
 
 const Weathercard = () => {
+    const weatherDetails = useSelector(state=>state.Weather.weatherdetails)
+    const weatherCondition = useSelector(state=>state.Weather.weatherCondition)
+    console.log(weatherDetails.icon)
+    const iconUrl = `http://openweathermap.org/img/wn/${weatherCondition.icon}@2x.png`
+    console.log(weatherDetails)
+    console.log(weatherCondition)
     return (
-        <div>
+        <MainDiv>
             <h1>City Name</h1>
             <img src="" alt="" />
-            <img src="" alt="" />
-            <h2>Temperature</h2>
+            <img src={iconUrl} alt="" />
+            <h2>Temperature:{weatherDetails.temp}</h2>
             <div className="max-min">
-                <h3>Max Temp</h3>
-                <h3>Min Temp</h3>
+                <h3>Max:{weatherDetails.temp_max}</h3>
+                <h3>Min:{weatherDetails.temp_min}</h3>
             </div>
-            <h3>Weather Condition</h3>
-        </div>
+            <h3>{weatherCondition.description}</h3>
+            </MainDiv>
     );
 };
+
+const MainDiv = styled.div`
+
+
+`
 
 export default Weathercard;
--- src/Components/home/home.js
+++ src/Components/home/home.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import CityAction from '../../Redux/Actions/CityAction';
 import {useDispatch,useSelector} from 'react-redux'
+import styled from 'styled-components'
 import Weathercard from './Weathercard';
 
 const Home = (e) => {
     const dispatch = useDispatch();
-    const weatherDetails = useSelector(state=>state.Weather.weatherdetails)
-    const weatherCondition = useSelector(state=>state.Weather.weatherCondition)
-    console.log(weatherCondition)
-    console.log(weatherDetails)
+
     const inputHandler =(e)=>{
         e.preventDefault();
         const cityName =e.target.a.value
@@ -17,13 +15,16 @@ const Home = (e) => {
         
     }
     return (
-        <div className='home'>
+        <Main>
             <form action="" onSubmit={inputHandler}>
                 <input name='a'  type="text" />
             </form>
             <Weathercard/>
-        </div>
+        </Main>
     );
 };
 
+const Main = styled.div`
+
+`
 export default Home;

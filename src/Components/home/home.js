5d3ac60567a197ff4f072c0fe646438f164d384b
Samir <pandeysamir529@gmail.com>
2022-04-16 11:56:58 +0545

    Version 1.2 changes(improved functionality of input field)

--- src/Components/home/home.js
+++ src/Components/home/home.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React,{useState} from 'react';
 import CityAction from '../../Redux/Actions/CityAction';
 import {useDispatch,useSelector} from 'react-redux'
 import styled from 'styled-components'
 import Weathercard from './Weathercard';
 
-const Home = (e) => {
+
+const Home = () => {
+    const[value,setValue] = useState("")
     const dispatch = useDispatch();
 
+    const input =(e)=> {
+    
+        setValue(e.target.value)
+
+    }
     const inputHandler =(e)=>{
         e.preventDefault();
         const cityName =e.target.a.value
-        dispatch(CityAction(cityName))
-        
-        
+        dispatch(CityAction(cityName))   
+        setValue("") 
     }
     return (
         <Main>
             <form action="" onSubmit={inputHandler}>
-                <input name='a'  type="text" />
+                <input name='a' value={value} onChange={input} type="text" />
             </form>
             <Weathercard/>
         </Main>
